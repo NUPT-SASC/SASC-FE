14a11a4c11d2975415ef8d11cf788a74ef83a1d7
gaoliang <gaoliangim@gmail.com>
2018-04-08 14:20:48 +0800

    update PublicPath for gh-pages

---
+++ src/pages/Timeline.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container" id="main_container">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style lang='less'>
+@import url('../styles/common.less');
+#main_container{
+    padding-top: 60px
+}
+
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/pages/Home';
 import Galley from '@/pages/Galley';
+import Timeline from '@/pages/Timeline';
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/galley',
       name: 'galley',
       component: Galley,
+    },
+    {
+      path: '/timeline',
+      name: 'timeline',
+      component: Timeline,
     },
   ],
 });
